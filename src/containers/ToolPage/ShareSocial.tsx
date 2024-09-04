9753f188e422be9829d53380957e50b62d0bc1cf
phamthainb <phamthai4643@gmail.com>
2024-09-04 23:37:14 +0700

    [master] update social share

---
+++ src/containers/ToolPage/ShareSocial.tsx
@@ -0,0 +1,87 @@
+import { DropDown } from "@/components/DropDown";
+import { handleShare, SocialPlatform } from "@/utils/social";
+import {
+  FaCopy,
+  FaFacebook,
+  FaLinkedin,
+  FaPinterest,
+  FaReddit,
+  FaShare,
+  FaTelegram,
+  FaTumblr,
+  FaTwitter,
+  FaWhatsapp,
+} from "react-icons/fa";
+import { TTool } from "./types";
+
+export default function ShareSocial({ data }: { data: TTool }) {
+  const content = `${data?.name} | ${data?.description}`.trim();
+  const currentUrl = window.location.origin;
+
+  return (
+    <DropDown
+      items={[
+        {
+          label: "Share on Facebook",
+          onClick: () =>
+            handleShare(SocialPlatform.Facebook, currentUrl, content),
+          icon: <FaFacebook size={"18px"} color="#3b5998" />,
+        },
+        {
+          label: "Share on Twitter/X",
+          onClick: () =>
+            handleShare(SocialPlatform.Twitter, currentUrl, content),
+          icon: <FaTwitter size={"18px"} color="#1DA1F2" />,
+        },
+        {
+          label: "Share on Telegram",
+          onClick: () =>
+            handleShare(SocialPlatform.Telegram, currentUrl, content),
+          icon: <FaTelegram size={"18px"} color="#0088cc" />,
+        },
+        {
+          label: "Share on WhatsApp",
+          onClick: () =>
+            handleShare(SocialPlatform.WhatsApp, currentUrl, content),
+          icon: <FaWhatsapp size={"18px"} color="#25D366" />,
+        },
+        {
+          label: "Share on LinkedIn",
+          onClick: () =>
+            handleShare(SocialPlatform.LinkedIn, currentUrl, content),
+          icon: <FaLinkedin size={"18px"} color="#0077b5" />,
+        },
+        {
+          label: "Share on Pinterest",
+          onClick: () =>
+            handleShare(SocialPlatform.Pinterest, currentUrl, content),
+          icon: <FaPinterest size={"18px"} color="#bd081c" />,
+        },
+        {
+          label: "Share on Reddit",
+          onClick: () =>
+            handleShare(SocialPlatform.Reddit, currentUrl, content),
+          icon: <FaReddit size={"18px"} color="#ff4500" />,
+        },
+        {
+          label: "Share on Tumblr",
+          onClick: () =>
+            handleShare(SocialPlatform.Tumblr, currentUrl, content),
+          icon: <FaTumblr size={"18px"} color="#35465C" />,
+        },
+        {
+          label: "Copy link",
+          onClick: () => {
+            navigator.clipboard.writeText(currentUrl);
+            alert("Link copied to clipboard!");
+          },
+          icon: <FaCopy size={"18px"} color="#000000" />,
+        },
+      ]}
+    >
+      <button className="flex gap-[6px] items-center justify-center w-full p-[5px] text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+        <FaShare size={"18px"} />
+      </button>
+    </DropDown>
+  );
+}
--- src/utils/social.ts
+++ src/utils/social.ts
@@ -1,3 +1,5 @@
+import toast from "react-hot-toast";
+
 export enum SocialPlatform {
   Facebook = "facebook",
   Twitter = "twitter",
@@ -7,7 +9,6 @@ export enum SocialPlatform {
   Pinterest = "pinterest",
   Reddit = "reddit",
   Tumblr = "tumblr",
-  Instagram = "instagram",
   Quora = "quora",
 }
 
@@ -20,6 +21,7 @@ export function handleShare(
 
   if (!validPlatforms.includes(platform)) {
     console.error(`Invalid platform: ${platform}`);
+    toast.error("Share got missing.");
     return;
   }
 
@@ -27,34 +29,50 @@ export function handleShare(
     [SocialPlatform.Facebook]: `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
       url
     )}&t=${text}`,
+
     [SocialPlatform.Twitter]: `https://twitter.com/intent/tweet?text=${encodeURIComponent(
       text || ""
     )}&url=${encodeURIComponent(url)}`,
+
     [SocialPlatform.Telegram]: `https://t.me/share/url?url=${encodeURIComponent(
       url
-    )}`,
+    )}&text=${encodeURIComponent(text || "")}`,
+
     [SocialPlatform.WhatsApp]: `https://wa.me/share?text=${encodeURIComponent(
       text || ""
     )}&url=${encodeURIComponent(url)}`,
+
     [SocialPlatform.LinkedIn]: `https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(
       url
     )}&title=${encodeURIComponent(text || "")}`,
+
     [SocialPlatform.Pinterest]: `https://www.pinterest.com/pin/create/button/?url=${encodeURIComponent(
       url
     )}&description=${encodeURIComponent(text || "")}`,
+
     [SocialPlatform.Reddit]: `https://www.reddit.com/submit?url=${encodeURIComponent(
       url
     )}&title=${encodeURIComponent(text || "")}`,
+
     [SocialPlatform.Tumblr]: `https://www.tumblr.com/share/link?url=${encodeURIComponent(
       url
     )}&name=${encodeURIComponent(text || "")}`,
-    [SocialPlatform.Instagram]: `https://www.instagram.com/?url=${encodeURIComponent(
-      url
-    )}`,
+
     [SocialPlatform.Quora]: `https://www.quora.com/q/add?url=${encodeURIComponent(
       url
     )}&title=${encodeURIComponent(text || "")}`,
   };
-
-  window.open(shareOptions[platform], "_blank");
 }
+
+// {
+//   "Facebook": "https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fcombokit.net%2Ftools%2Fimage-optimization&t=Image%20Compression",
+//   "Twitter": "https://twitter.com/intent/tweet?text=Image%20Compression&url=https%3A%2F%2Fcombokit.net%2Ftools%2Fimage-optimization",
+//   "Telegram": "https://t.me/share/url?url=https%3A%2F%2Fcombokit.net%2Ftools%2Fimage-optimization&text=Image%20Compression",
+//   "WhatsApp": "https://wa.me/share?text=Image%20Compression&url=https%3A%2F%2Fcombokit.net%2Ftools%2Fimage-optimization",
+//   "LinkedIn": "https://www.linkedin.com/shareArticle?mini=true&url=https%3A%2F%2Fcombokit.net%2Ftools%2Fimage-optimization&title=Image%20Compression",
+//   "Pinterest": "https://www.pinterest.com/pin/create/button/?url=https%3A%2F%2Fcombokit.net%2Ftools%2Fimage-optimization&description=Image%20Compression",
+//   "Reddit": "https://www.reddit.com/submit?url=https%3A%2F%2Fcombokit.net%2Ftools%2Fimage-optimization&title=Image%20Compression",
+//   "Tumblr": "https://www.tumblr.com/share/link?url=https%3A%2F%2Fcombokit.net%2Ftools%2Fimage-optimization&name=Image%20Compression",
+//   "Instagram": "https://www.instagram.com/?url=https%3A%2F%2Fcombokit.net%2Ftools%2Fimage-optimization",
+//   "Quora": "https://www.quora.com/q/add?url=https%3A%2F%2Fcombokit.net%2Ftools%2Fimage-optimization&title=Image%20Compression"
+// }

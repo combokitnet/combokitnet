2ce1097747d6be2a6c26ab4966c7bc913c0ddf31
ComboKit.Net <143339627+combokitnet@users.noreply.github.com>
2024-10-18 16:59:23 +0700

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,15 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        
+         {/* Microsoft Clarity */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "okhyrqoyd1");`,
+          }}
+        />
+        
         <Script
           src="https://the.gatekeeperconsent.com/cmp.min.js"
           data-cfasync="false"

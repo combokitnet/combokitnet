982906e00495c1b1089e9597d9393cdcc0d49c4f
phamthainb <phamthai4643@gmail.com>
2025-01-01 02:48:16 +0700

    [master] chplay api update

--- src/pages/api/chplay.ts
+++ src/pages/api/chplay.ts
@@ -1,29 +1,20 @@
 import axios from "axios";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
   runtime: "experimental-edge",
 };
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { id } = req.query;
-
-  if (!id) {
-    return res.status(400).json({ error: "Missing app id" });
-  }
+export default async function handler(request: NextRequest, event: any) {
+  console.log("context", request.url);
+  const query = request.url?.split("api/chplay")[1];
 
   try {
     const response = await axios.get(
-      `https://play.google.com/store/apps/details`,
-      {
-        params: req?.query,
-      }
+      `https://play.google.com/store/apps/details${query}`
     );
 
-    const html = response.data;
+    const html = response?.data;
 
     // Regular expression to extract specific data from the HTML
     const descriptionMatch = html.match(
@@ -35,12 +26,20 @@ export default async function handler(
 
     const appDetails = {
       description,
-      ...req?.query,
     };
-
-    res.status(200).json(appDetails);
+    return NextResponse.json({
+      message: "ok",
+      query,
+      ...appDetails,
+      success: true,
+    });
   } catch (error) {
     console.error("Error fetching app details:", error);
-    res.status(500).json({ error: "Failed to fetch app details" });
+
+    return NextResponse.json({
+      message: "Error fetching app details",
+      query,
+      success: false,
+    });
   }
 }
